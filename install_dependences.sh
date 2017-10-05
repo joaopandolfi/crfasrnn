058e0dad1bea981d9a78d6216b383ab72d5712e3
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2017-10-05 00:50:35 -0300

    Install Scripts Update

--- install_dependences.sh
+++ install_dependences.sh
@@ -1,3 +1,4 @@
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install --no-install-recommends libboost-all-dev
-
+sudo apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
+sudo apt-get install libatlas-base-dev 
